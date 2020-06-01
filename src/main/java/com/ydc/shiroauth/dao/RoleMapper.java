41dbbd48318d3ab4167c7c2261d16e83187061f2
“ydcgit config --global user.email yang.2007.4@163.comgit initgit config --global user.name “ydc <yang.2007.4@163.com>
2020-06-01 09:32:16 +0800

    update

--- src/main/java/com/ydc/shiroauth/dao/RoleMapper.java
+++ src/main/java/com/ydc/shiroauth/dao/RoleMapper.java
@@ -1,24 +1,35 @@
 package com.ydc.shiroauth.dao;
 
-import com.ydc.shiroauth.po.Role;
-import com.ydc.shiroauth.po.User;
-import org.apache.ibatis.annotations.*;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.mapping.FetchType;
 
-import java.util.List;
+import com.ydc.shiroauth.po.Role;
 
 @Mapper
 public interface RoleMapper {
-    @Select("SELECT r.* FROM role LEFT JOIN user_role ur ON r.id=ur.role_id" +
-            "WHERE ur.user_id=#{userId}")
-    @Results({@Result(column = "role_id", property = "permissions", many = @Many(select = "com.ydc.shiroauth.dao.PermissionMapper.getByRole", fetchType = FetchType.LAZY))})
+    @Select("SELECT r.* FROM role LEFT JOIN user_role ur ON r.id=ur.role_id"
+            + "WHERE ur.user_id=#{userId}")
+    @Results({@Result(column = "role_id", property = "permissions",
+            many = @Many(select = "com.ydc.shiroauth.dao.PermissionMapper.getByRole",
+                    fetchType = FetchType.LAZY))})
     List<Role> getByUser(String userId);
 
-    @SelectKey(keyProperty = "id", before = true, statement = "select uuid()", resultType = String.class)
+    @SelectKey(keyProperty = "id", before = true, statement = "select uuid()",
+            resultType = String.class)
     @Insert("INSERT INTO `role` (`id`, `name`, `code`) VALUES (#{id}, #{name}, #{code})")
     void insert(Role role);
 
-    @SelectKey(keyProperty = "id", before = true, statement = "select uuid()", resultType = String.class)
+    @SelectKey(keyProperty = "id", before = true, statement = "select uuid()",
+            resultType = String.class)
     @Insert("INSERT INTO `user_role` (`id`, `user_id`, `role_id`) VALUES (#{id}, #{userId}, #{roleId})")
-    void insertUserRole(@Param("roleId")String roleId, @Param("userId") String userId);
+    void insertUserRole(@Param("roleId") String roleId, @Param("userId") String userId);
 }
--- src/main/java/com/ydc/shiroauth/dao/UserMapper.java
+++ src/main/java/com/ydc/shiroauth/dao/UserMapper.java
@@ -1,8 +1,15 @@
 package com.ydc.shiroauth.dao;
 
-import com.ydc.shiroauth.po.User;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.mapping.FetchType;
+
+import com.ydc.shiroauth.po.User;
 
 @Mapper
 public interface UserMapper {
@@ -10,12 +17,13 @@ public interface UserMapper {
     User get(String id);
 
     @Select("select * from user where account=#{account}")
-    @Results({
-            @Result(column = "user_id", property = "roles", many = @Many(select = "com.ydc.shiroauth.dao.RoleMapper.getByUser", fetchType = FetchType.LAZY)),
-    })
+    @Results({@Result(column = "user_id", property = "roles",
+            many = @Many(select = "com.ydc.shiroauth.dao.RoleMapper.getByUser",
+                    fetchType = FetchType.LAZY))})
     User getByAccount(String account);
 
-    @SelectKey(keyProperty = "id", before = true, statement = "select uuid()", resultType = String.class)
+    @SelectKey(keyProperty = "id", before = true, statement = "select uuid()",
+            resultType = String.class)
     @Insert("INSERT INTO `user` (`id`, `name`, `account`, `password`, `sex`) VALUES (#{id},#{name},#{account},#{password},#{sex})")
     void insert(User user);
 }
